ed864a0700f1a79aeae1a5bbce8f130614fb74fe
CB_sjtu <cb_sjtu@sjtu.edu.cn>
2025-07-10 16:28:14 +0800

    ignore pycache

--- getdata.py
+++ getdata.py
@@ -13,13 +13,12 @@ def get_data():
     # 低精度数据：只取 [0,1,3,4] 为训练集，reshape 成 [4, 200]
     uum_1d_new = uum_1d[[0, 1, 3, 4], :200].astype(np.float32)
     uum_1d_new_test = uum_1d[[2], :200].astype(np.float32)
-
-    trunk_out = np.load('data_gen/data_motai_3d_mix.npy')[[0, 1, 3, 4, 5, 6, 8, 9]].reshape((4, 200, -1))
-    trunk_out_test = np.load('data_gen/data_motai_3d_mix.npy')[[2, 7]].reshape((1, 200, -1))
+    trunk_out = np.load('data_gen/data_motai_3d_mix.npy')[[0, 1, 3, 4, 5, 6, 8, 9]].reshape((8, 200, -1))
+    trunk_out_test = np.load('data_gen/data_motai_3d_mix.npy')[[2, 7]].reshape((2, 200, -1))
     motai = trunk_out.transpose(0, 2, 1)
     motai_test = trunk_out_test.transpose(0, 2, 1)
-    branch_in = np.concatenate((trunk_out.real[..., np.newaxis], trunk_out.imag[..., np.newaxis]), axis=-1).astype(np.float32).reshape((4, 200, -1))
-    branch_in_test = np.concatenate((trunk_out_test.real[..., np.newaxis], trunk_out_test.imag[..., np.newaxis]), axis=-1).astype(np.float32).reshape((1, 200, -1))
+    branch_in = np.concatenate((trunk_out.real[..., np.newaxis], trunk_out.imag[..., np.newaxis]), axis=-1).astype(np.float32).reshape((8, 200, -1))
+    branch_in_test = np.concatenate((trunk_out_test.real[..., np.newaxis], trunk_out_test.imag[..., np.newaxis]), axis=-1).astype(np.float32).reshape((2, 200, -1))
     
     for i in range(8):
         scaler_Euuc = StandardScaler().fit(branch_in[i])
@@ -39,13 +38,13 @@ def get_data():
     kzs_s_test = kzs[[2]]
     kzs_s_test = 2 * math.pi / kzs_s_test * 1000.512
 
-    dcPs_s = np.load('data_gen/data_dcp23.npy').astype(np.float32)[[0, 1, 3, 4, 5, 6, 8, 9]].reshape((-1,  23))
-    dcPs_s_test = np.load('data_gen/data_dcp23.npy').astype(np.float32)[[2, 7]].reshape((-1,  23))
-    dkxs_s = np.load('data_gen/data_dkx86.npy').astype(np.float32)[[0, 1, 3, 4, 5, 6, 8, 9]].reshape((-1, 86))
-    dkxs_s_test = np.load('data_gen/data_dkx86.npy').astype(np.float32)[[2,7]].reshape((-1, 86))
+    dcPs_s = np.load('data_gen/data_dcp23_mix.npy').astype(np.float32)[[0, 1, 3, 4, 5, 6, 8, 9]].reshape((-1,  23))
+    dcPs_s_test = np.load('data_gen/data_dcp23_mix.npy').astype(np.float32)[[2, 7]].reshape((-1,  23))
+    dkxs_s = np.load('data_gen/data_dkx86_mix.npy').astype(np.float32)[[0, 1, 3, 4, 5, 6, 8, 9]].reshape((-1, 86))
+    dkxs_s_test = np.load('data_gen/data_dkx86_mix.npy').astype(np.float32)[[2,7]].reshape((-1, 86))
  
     y = np.load('data_gen/data_yy.npy')
-    y=1-y
+    # y=1-y
     real_2d_y = y
     real_2d_x = np.zeros((5, 87))
     real_2d_x[[0, 1, 3, 4]] = kzs_s.reshape((4, 87))
@@ -80,4 +79,7 @@ def get_data():
     coodinates_zz = coodinates[[0, 1, 3, 4, 5, 6, 8, 9]]
     coodinates_zz_test = coodinates[[2,7]]
     
-    return uum_new, uum_new_test, branch_in, branch_in_test, dcPs_s, dcPs_s_test, dkxs_s, dkxs_s_test, kzs_s, kzs_s_test, y, y_test, real_2d, kzs, motai, motai_test, coodinates_zz, coodinates_zz_test
+    return uum_new, uum_new_test, branch_in, branch_in_test, \
+        dcPs_s, dcPs_s_test, dkxs_s, dkxs_s_test, kzs_s, kzs_s_test, \
+        y, y_test, real_2d, kzs, motai, motai_test, \
+        coodinates_zz, coodinates_zz_test, uum_1d_new, uum_1d_new_test
